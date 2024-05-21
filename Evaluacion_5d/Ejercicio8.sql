a2ca968b1c4426a4ffb18d5ba911c4190f130d6f
valentinadutoya <laravalentinadutoya@gmail.com>
2024-05-21 14:51:18 -0300

    cambie un signo en el ejercicio 8

--- Evaluacion_5d/Ejercicio8.sql
+++ Evaluacion_5d/Ejercicio8.sql
@@ -1,7 +1,7 @@
 SELECT e.event_name, count(e.person_id)FROM facebook_event_checkin e
 JOIN person p ON p.id = e.person_id
 JOIN income s ON p.ssn = s.ssn
-WHERE annual_income < "35000"
+WHERE annual_income > "35000"
 GROUP BY event_id
 ORDER BY COUNT(person_id) DESC
 LIMIT 5
